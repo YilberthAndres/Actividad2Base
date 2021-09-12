b7de64c1c5725d450b525b2c04d7c6f965abebe7
israel25D <ijesusdiaz@uniguajira.edu.co>
2021-09-12 17:50:36 -0500

    israel

--- library/class_desarrollo.py
+++ library/class_desarrollo.py
@@ -1,4 +1,4 @@
-
+from library.mod_limpiar import *
 class Desarrolloo():
     def __init__(self):
         self.__personas = {}
@@ -10,7 +10,9 @@ class Desarrolloo():
     
     def mostraspersonas(self):
         for i in self.__listapersonas:
-            print(f"Nombre {i[0]} Apellido {i[1]} Telefono {i[2]} y Codigo {i[3]} ")
+            print(f"Nombre: {i[0]} Apellido: {i[1]} Telefono: {i[2]} Codigo: {i[3]} ")
+
+
     
     def eliminarpersona(self):
         borrar = False
@@ -20,3 +22,4 @@ class Desarrolloo():
                 self.__listapersonas.remove(i)
                 borrar = True
                 input("Fue borrado exitosamente, pulse cualquier tecla")
+                limpiar_pantalla()
--- library/mod_nombre.py
+++ library/mod_nombre.py
@@ -1,4 +1,4 @@
-
+from library.mod_limpiar import *
 from library.class_desarrollo import *
 
 E1 = Desarrolloo()
@@ -10,17 +10,23 @@ def cargar():
     print("---- AGREGAR A UNA PERSONA ----")
     # archivo = open("library/base.csv","a")
     while continua == "s":
-        no = input("ingrese su nombre por favor ---> ")
-        ap = input("ingrese su apellido por favor --> ")
-        tel = input("ingrese su telefono por favor --> ")
-        cod = input("ingrese su codigo por favor --> ")
-        continua = input("desea continuar s/n  ")
+        no = input("ingrese su nombre por favor: ")
+        print("-----------------------------------")
+        ap = input("ingrese su apellido por favor: ")
+        print("-----------------------------------")
+        tel = input("ingrese su telefono por favor: ")
+        print("-----------------------------------")
+        cod = input("ingrese su codigo por favor: ")
+        print("-----------------------------------")
+        continua = input("desea continuar s/n:  ")
         print(continua)
         E1.datosguardados(no,ap,tel,cod)
+        limpiar_pantalla()
     return amig
     
 def mostrarr():
     E1.mostraspersonas()
+    
 def eliminar():
     E1.eliminarpersona()
 
