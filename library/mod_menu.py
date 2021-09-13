532da86eef51c6dd26d7eafdcc1f597152035f90
israel <israel>
2021-09-13 00:29:02 -0500

    israel

--- library/mod_menu.py
+++ library/mod_menu.py
@@ -39,8 +39,8 @@ def opciones():
             op = int(input("Que opcion elijes: "))
             print("")
 
-            if(op < 1 or op > 3):
-                print("Por favor elija una opcion entre 1 y 3")
+            if(op < 1 or op > 4):
+                print("Por favor elija una opcion entre 1 y 4")
                           
             if(op == 1):
               op = 0
@@ -51,6 +51,9 @@ def opciones():
             if(op ==3):
               op = 0
               mostrarr()
+            if(op == 4):
+              op = 0
+              opciones()
 
     if(op == 2):
         limpiar_pantalla()
